08e224d2af84dbd17de67eaee08e59423105faf3
balooin <himansu1300@gmail.com>
2025-03-07 17:53:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript for handling the mobile menu toggle
-const menuToggle = document.getElementById('menuToggle');
-const navMenu = document.getElementById('navMenu');
-
-menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
